ea97c4f469dcee5389e956659924038187bc36ae
Shrimp-Chip <esample21@gmail.com>
2024-06-26 19:00:02 -0400

    Creating programs via shader sets

--- rxtion/rx_core/rendering/shader.h
+++ rxtion/rx_core/rendering/shader.h
@@ -7,7 +7,8 @@
 #define RXCORE_SHADER_DEBUG
 
 #ifdef RXCORE_SHADER_DEBUG
-#define RXCORE_SHADER_DEBUG_PRINT(str, ...) gs_println("RXCORE::rendering::shader::" str, __VA_ARGS__)
+#define RXCORE_SHADER_DEBUG_PRINT(str) gs_println("RXCORE::rendering::shader::" str)
+#define RXCORE_SHADER_DEBUG_PRINTF(str, ...) gs_println("RXCORE::rendering::shader::" str, __VA_ARGS__)
 #else
 #define RXCORE_SHADER_DEBUG_PRINT(...) ((void)0)
 #endif
@@ -46,9 +47,15 @@ typedef struct rxcore_shader_registry_t
 
 typedef struct rxcore_shader_set_t
 {
-    uint16_t vertex_shader_id;
-    uint16_t fragment_shader_id;
+    rxcore_shader_t *vertex_shader;
+    rxcore_shader_t *fragment_shader;
 } rxcore_shader_set_t;
+
+typedef struct rxcore_shader_program_t
+{
+    const char *program_name;
+    gs_handle(gs_graphics_shader_t) program;
+} rxcore_shader_program_t;
 
 // RXCORE_SHADER public methods
 rxcore_shader_desc_t rxcore_shader_desc_create(const char *shader_name, const char *shader_path, rxcore_shader_stage_t stage, const char **shader_dependencies, uint16_t shader_dependency_count);
@@ -69,5 +76,10 @@ void rxcore_shader_registry_destroy(rxcore_shader_registry_t *reg);
 // RXCORE_SHADER_REGISTRY private methods
 rxcore_shader_t *_rxcore_shader_registry_find_dependency(rxcore_shader_registry_t *reg, const char *shader_name);
 rxcore_shader_t *_rxcore_shader_registry_find_shader(rxcore_shader_registry_t *reg, const char *shader_name);
+
+// RXCORE_SHADER_SET public methods
+rxcore_shader_program_t *rxcore_shader_program_set(rxcore_shader_set_t set);
+void rxcore_shader_program_destroy(rxcore_shader_program_t *program);
+
 
 #endif // __SHADER_H__
