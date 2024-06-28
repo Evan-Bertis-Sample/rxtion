d60778b37e4a4ec4b882e072889dee07a3e4e308
Shrimp-Chip <esample21@gmail.com>
2024-06-28 19:29:27 -0400

    Began creating materials

---
+++ rxtion/rx_core/rendering/material.h
@@ -0,0 +1,51 @@
+#ifndef __MATERIAL_H__
+#define __MATERIAL_H__
+
+#include <stdint.h>
+#include <rx_core/rendering/shader.h>
+#include <gs/gs.h>
+
+typedef struct rxcore_material_prototype_t
+{
+    rxcore_shader_set_t shader_set;
+    gs_graphics_uniform_desc_t *uniform_descs;
+    uint32_t num_uniforms;
+} rxcore_material_prototype_t;
+
+typedef struct rxcore_material_uniforms_t
+{
+    rxcore_shader_stage_t stage;
+    gs_handle(gs_graphics_uniform_t) *uniform_handles;
+    gs_graphics_bind_uniform_desc_t *uniform_bindings;
+    uint32_t num_uniforms;
+} rxcore_material_uniforms_t;
+
+typedef struct rxcore_material_t
+{
+    rxcore_shader_set_t shader_set;
+    rxcore_material_uniforms_t *vertex_uniforms;
+    rxcore_material_uniforms_t *fragment_uniforms;
+} rxcore_material_t;
+
+// RXCORE_MATERIAL_PROTOTYPE methods
+rxcore_material_prototype_t rxcore_material_prototype_create_impl(rxcore_shader_set_t set, gs_graphics_uniform_desc_t *uniform_descs, uint32_t num_uniforms);
+#define rxcore_material_prototype_create(set, ...) rxcore_material_prototype_create_impl(set, (gs_graphics_uniform_desc_t[]){__VA_ARGS__}, sizeof((gs_graphics_uniform_desc_t[]){__VA_ARGS__}) / sizeof(gs_graphics_uniform_desc_t)
+
+// RXCORE_MATERIAL_UNIFORMS methods
+rxcore_material_uniforms_t rxcore_material_uniforms_create_impl(rxcore_shader_stage_t stage, gs_handle(gs_graphics_uniform_t) *uniform_handles, gs_graphics_bind_uniform_desc_t *uniform_bindings, uint32_t num_uniforms);
+
+
+// RXCORE_MATERIAL methods
+rxcore_material_t *rxcore_material_create(const rxcore_material_prototype_t *prototype);
+void rxcore_material_add_binding(rxcore_material_t *material, const char *uniform_name, void *data, uint32_t size);
+
+#define rxcore_uniform_desc(uniform_stage, uniform_name, uniform_type) \
+    { \
+        .stage = uniform_stage, \
+        .name = uniform_name, \
+        .layout = { \
+            .type = uniform_type \
+        } \
+    }
+
+#endif // __MATERIAL_H__
