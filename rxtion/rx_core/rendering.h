bdb932242ad0f461bd558bbc54e32416ecc4e446
evan-bertis-sample <esample21@gmail.com>
2024-07-01 00:09:48 -0400

    Mesh primatives

--- rxtion/rx_core/rendering.h
+++ rxtion/rx_core/rendering.h
@@ -5,6 +5,8 @@
 #include <gs/gs.h>
 #include <rx_core/rendering/shader.h>
 #include <rx_core/rendering/material.h>
+#include <rx_core/rendering/mesh.h>
+#include <rx_core/rendering/mesh_primatives.h>
 
 #define CORE_ASSET(ASSET_NAME) "rxtion/rx_core/" ASSET_NAME
 #define APP_ASSET(ASSET_NAME) "rxtion/rx_app/assets/" ASSET_NAME
@@ -67,31 +69,92 @@ void rxcore_rendering_init()
 
     gs_println("Creating material prototype");
     gs_graphics_uniform_desc_t uni_test = RXCORE_UNIFORM_DESC("u_test", RXCORE_SHADER_STAGE_FRAGMENT, GS_GRAPHICS_UNIFORM_INT);
-    
 
     gs_println("Creating material from prototype");
     rxcore_material_prototype_t prototype = RXCORE_MATERIAL_PROTOTYPE_CREATE(
-        set, 
+        set,
         RXCORE_UNIFORM_DESC("u_test_float", RXCORE_SHADER_STAGE_VERTEX, GS_GRAPHICS_UNIFORM_FLOAT),
-        uni_test
-    );
+        uni_test);
 
     gs_println("Creating material from prototype");
     rxcore_material_t *material = RXCORE_MATERIAL_CREATE_FROM_PROTOTYPE_OVERIDE(&prototype, uni_test);
 
     rxcore_material_print(material);
-    
+
     gs_println("Adding bindings");
     int binding = 5;
     float float_binding = 5.0f;
     rxcore_material_add_binding(material, "u_test", &binding, sizeof(int));
     rxcore_material_add_binding(material, "u_test_float", &float_binding, sizeof(float));
 
+    gs_println("Testing meshes");
+
+    rxcore_mesh_registry_t *mesh_reg = rxcore_mesh_registry_create();
+    rxcore_vertex_t *vertices = NULL;
+    uint32_t *indices = NULL;
+    uint32_t vertex_count = 0;
+    uint32_t index_count = 0;
+
+    rxcore_mesh_primatives_quad(&vertices, &vertex_count, &indices, &index_count);
+    rxcore_mesh_registry_add_mesh(mesh_reg, "quad", vertices, vertex_count, indices, index_count);
+
+    // free the memory
+    free(vertices);
+    free(indices);
+
+    rxcore_mesh_primatives_triangle(&vertices, &vertex_count, &indices, &index_count);
+    rxcore_mesh_registry_add_mesh(mesh_reg, "triangle", vertices, vertex_count, indices, index_count);
+
+    // free the memory
+    free(vertices);
+    free(indices);
+
+    rxcore_mesh_buffer_get_vertex_buffer(mesh_reg->buffer);
+    rxcore_mesh_buffer_get_index_buffer(mesh_reg->buffer);
+
+    // get the mesh
+    rxcore_mesh_t mesh = rxcore_mesh_registry_get_mesh(mesh_reg, "quad");
+    // print out the mesh
+    rxcore_vertex_t *mesh_vertices = rxcore_mesh_get_vertices(&mesh);
+    uint32_t *mesh_indices = rxcore_mesh_get_indices(&mesh);
+
+    gs_println("Quad:");
+    for (uint32_t i = 0; i < mesh.index_count; i++)
+    {
+        gs_println("  Vertex: %f, %f, %f", mesh_vertices[i].position.x, mesh_vertices[i].position.y, mesh_vertices[i].position.z);
+    }
+
+    for (uint32_t i = 0; i < mesh.index_count; i++)
+    {
+        gs_println("  Index: %d", mesh_indices[i]);
+    }
+
+    // free both the vertices and indices
+    free(mesh_vertices);
+    free(mesh_indices);
+
+    gs_println("\nTriangle:");
+    // now do the same for the triangle
+    mesh = rxcore_mesh_registry_get_mesh(mesh_reg, "triangle");
+    // print out the mesh
+    mesh_vertices = rxcore_mesh_get_vertices(&mesh);
+    mesh_indices = rxcore_mesh_get_indices(&mesh);
+
+    for (uint32_t i = 0; i < mesh.index_count; i++)
+    {
+        gs_println("  Vertex: %f, %f, %f", mesh_vertices[i].position.x, mesh_vertices[i].position.y, mesh_vertices[i].position.z);
+    }
+
+    for (uint32_t i = 0; i < mesh.index_count; i++)
+    {
+        gs_println("  Index: %d", mesh_indices[i]);
+    }
+
+    gs_println("Finished init");
 }
 
 void rxcore_rendering_update()
 {
-
 }
 
 void rxcore_rendering_shutdown()
